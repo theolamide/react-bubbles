332cdf88935796d70786a943fb8acd093e6fcb3b
theolamide <ajadidan@gmail.com>
2019-11-15 12:27:07 -0500

    Bubbles page get request successful

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -3,11 +3,19 @@ import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import { axiosWithAuth } from "./Utils/axiosWithAuth";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  axiosWithAuth()
+  .get('/colors')
+  .then(res => {
+    console.log(res.data)
+    setColorList(res.data)
+  })
 
   return (
     <>
